6407fae3f57c9e50db8b5253527a60299b74cd18
azez-alhoot <azezalhoot82@yahoo.com>
2020-06-07 14:43:12 +0300

    add the page store css&html&js

--- js/store.js
+++ js/store.js
@@ -1,17 +1,16 @@
 'use strict'
 
-var productsNames = [];
-
 var products = [];
-function Products(name, path, price){
+function Products(name, path, price, quantity){
 this.name = name;
 this.path = path;
 this.price = price;
+this.quantity = quantity;
 products.push(this);
 }
 
-for(var i=0; i<productsNames.length; i++){
-    new Products(productsNames[i]);
+for(var i=0; i<products.length; i++){
+    new Products(products[i]);
 }
 
 
@@ -19,19 +18,35 @@ var addToCart = document.getElementsByClassName('container');
 addEventListener('click',handelAddToCart);
 
 function handelAddToCart (event){
+    getProduct();
     if(event.target.textContent == 'Add To Cart'){
     // Reach the path of the product image
     var productPath = event.path[2].children[0].children[0].attributes[0].nodeValue;
     console.log(productPath);
     // Reach the name of the product
-    var poductName = event.path[1].children[0].childNodes[0].textContent;
+    var poductName = event.path[2].children[0].children[1].textContent;
      console.log(poductName);
      console.log(event.path[2].children[0].children[0].attributes[1].nodeValue);
      // Reach the price of the product
     var productPrice = event.path[2].children[0].children[0].attributes[1].nodeValue; 
+    //Reach the quantity
+    var productQuantity = event.path[1].children[1].value;
+    //creat p
+    new Products(poductName, productPath, productPrice, productQuantity);
 
-    new Products(poductName, productPath, productPrice);
-
-    console.log(products);
+    // console.log(products);
+    setProduct();
     }
+}
+//send the selected Product to the local srorage
+function setProduct(){
+    var item = JSON.stringify(products);
+    localStorage.setItem('item', item);
+}
+
+function getProduct(){
+  var getproduct = localStorage.getItem('item');
+  if(getproduct){
+  products = JSON.parse(getproduct);
+  }
 }
