b14ff900530f0be68e7432099362e01c5c223c42
laith <mr.layth_2011@hotmail.com>
2020-06-09 16:36:10 +0300

    styleContactAndDonate

--- js/donate.js
+++ js/donate.js
@@ -1,15 +1,70 @@
-// Nav 
+'use strict';
+
+//constructor for donate
+// function Donate(donateName, mony) {
+//   this.donateName = donateName;
+//   this.mony = mony;
+//   allDonat.all.push(this);
+// }
+
+// Donate.all = [];
+
+// Donate.prototype.addItem = function (donateName, mony) {
+//   var thisDonate = new Donate(donateName, mony)
+//   this.donateName.push(thisDonate);
+//   this.mony.push(thisDonate);
+// };
+
+
+//store data to local storage
+// Donate.prototype.storeToLocalStorage = function () {
+
+//   var store = JSON.stringify(Donate.all);
+//   localStorage.setItem('allDonate', store);
+// }
+
+
+//get data from local storage
+// Donate.prototype.getDataFromeLocalStorage = function () {
+
+//   var store = localStorage.getItem('allDonate');
+//   if (store) {
+//     Donate.all = JSON.parse(store);
+
+//   }
+// }
+
+// on click to submit butoon
+var donationform = document.querySelector('#donationform');
+donationform.addEventListener('submit', buttonsub);
+
+function buttonsub() {
+  event.preventDefault();
+  alert('thank you for Donate');
+  donationform.reset();
+}
+
+// var donationform = document.getElementById("donationform");
+// guestForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     var donorNam = event.target.doname.value;
+//     var donorMon = event.target.money.value;
+// })
+
+
+
+
 // this for the nav link
-var links=document.getElementById("icon");
-links.addEventListener('click',function(event){
-    event.preventDefault();
-    
-    var displayOrHide=document.getElementById("myLinks")
-    if (displayOrHide.style.display === "inline-block") {
-        displayOrHide.style.display = "none";
-    } else {
-        displayOrHide.style.display = "inline-block";
-    }
+var links = document.getElementById("icon");
+links.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var displayOrHide = document.getElementById("myLinks")
+  if (displayOrHide.style.display === "inline-block") {
+    displayOrHide.style.display = "none";
+  } else {
+    displayOrHide.style.display = "inline-block";
+  }
 });
 // end nav link
 // for make nav fixed and give him opecity 0.5 after scroll
@@ -25,3 +80,4 @@ function scrollFunction() {
   }
 }
 // End Nav functions
+
